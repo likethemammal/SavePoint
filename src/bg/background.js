910644f0ed360f0adb09c56fda9884c941669db5
Chris Dolphin <cdolphin1991@gmail.com>
2013-08-10 18:02:07 -0400

    I DID ITgit add . First functionality working

--- src/bg/background.js
+++ src/bg/background.js
@@ -1,4 +1,9 @@
+
+var code = "(function(){var currentScroll = window.scrollY;if (window.lastScroll){window.scrollTo(0, window.lastScroll);} else {window.lastScroll = currentScroll;}}());"
+
+// var code = 'console.log(window.scrollY)'
+
 chrome.browserAction.onClicked.addListener(function(tab) {
 	console.log(chrome.tabs);
-	chrome.tabs.executeScript(null, {code: 'console.log(window)'});
+	chrome.tabs.executeScript(null, {code: code});
 });
