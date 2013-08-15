5a5d1697093d1d4740052edd80e2ae6ab1121dd0
Chris Dolphin <cdolphin1991@gmail.com>
2013-08-15 17:54:55 -0400

    Error handling and multiple window checking.

--- src/bg/background.js
+++ src/bg/background.js
@@ -2,7 +2,7 @@
 
 chrome.browserAction.onClicked.addListener(function(tab) {
 	console.log('Button Clicked');
-	chrome.tabs.executeScript(null, {file: '/src/js/onSaveClick.js'});
+	chrome.tabs.executeScript(null, {file: '/src/js/onSaveClick.js'}, handleError);
 });
 
 chrome.extension.onRequest.addListener(function(color){
@@ -25,3 +25,23 @@ chrome.tabs.onUpdated.addListener(function(tabID, changeInfo, tab){
 		console.log(chrome.browserAction.onClicked);
 	}
 });
+
+chrome.windows.onFocusChanged.addListener(function(){
+    console.log('Window Changed');
+    chrome.tabs.executeScript(null, {file: "/src/js/clearData.js"});
+    chrome.tabs.executeScript(null, {file: "/src/js/checkTab.js"});
+    chrome.tabs.executeScript(null, {file: "/src/js/onActivate.js"}, function() {
+        chrome.browserAction.setIcon({path: '/src/icons/icon16.png'});
+    });
+});
+
+chrome.commands.onCommand.addListener(function(command) {
+    console.log('Keyboard shortcut used: ', command);
+    chrome.tabs.executeScript(null, {file: '/src/js/onSaveClick.js'}, handleError);
+});
+
+function handleError(results) {
+    if (results === undefined) {
+        chrome.browserAction.setIcon({path: '/src/icons/icon16r.png'});
+    }
+}
